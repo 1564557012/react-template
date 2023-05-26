46d48397fb93705a7766c82d3d9026f11d4e1e91
Sefri <1564557012@qq.com>
2023-05-26 16:28:52 +0800

    :tada: init: init

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// import 'antd/dist/reset.css'
+const app = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+app.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
